b1f207a36c469976b95ce3f492e431579de08c0d
lutr <t.luca1152@gmail.com>
2024-06-02 22:40:54 +0300

    feat(04): make the camera look at the mesh

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -36,8 +36,10 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.z = 3;
 scene.add(camera)
+
+camera.lookAt(mesh.position);
 
 /**
  * Renderer
