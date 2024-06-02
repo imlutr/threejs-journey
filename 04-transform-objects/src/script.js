b7af055a2d3af02df0d4c1de42bc1897397b5f93
lutr <t.luca1152@gmail.com>
2024-06-02 18:09:36 +0300

    feat(04): scale the mesh

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -15,9 +15,10 @@ const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
 // Position
-mesh.position.x = 0.8
-mesh.position.y = -0.6
-mesh.position.z = 1
+mesh.position.set(0.8, -0.6, 1)
+
+// Scale
+mesh.scale.set(2, 0.5, 0.5)
 
 /**
  * Sizes
