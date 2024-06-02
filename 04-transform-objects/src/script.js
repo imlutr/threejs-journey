b2d9662e365463fa5d42705122e761368b82dede
lutr <t.luca1152@gmail.com>
2024-06-02 22:53:14 +0300

    feat(04): render a group of 3 cubes

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -9,20 +9,31 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({color: 0xff0000})
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = Math.PI / 4;
+scene.add(group)
 
-// Position
-mesh.position.set(0.8, -0.6, 1)
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
 
-// Scale
-mesh.scale.set(2, 0.5, 0.5)
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube2.position.x = -2
+group.add(cube2)
 
-// Rotation
-mesh.rotation.y = Math.PI * 0.25
-mesh.rotation.x = Math.PI * 0.25
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube3.position.x = 2
+group.add(cube3)
 
 /**
  * Sizes
@@ -38,8 +49,6 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3;
 scene.add(camera)
-
-camera.lookAt(mesh.position);
 
 /**
  * Renderer
