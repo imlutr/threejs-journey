d22fde339e60144cd1e4c806c02f72e7097f6f5f
lutr <t.luca1152@gmail.com>
2024-06-02 18:06:59 +0300

    feat(04): position the mesh

--- 04-transform-objects/src/script.js
+++ 04-transform-objects/src/script.js
@@ -10,16 +10,21 @@ const scene = new THREE.Scene()
  * Objects
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({color: 0xff0000})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+// Position
+mesh.position.x = 0.8
+mesh.position.y = -0.6
+mesh.position.z = 1
 
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+  width: 800,
+  height: 600
 }
 
 /**
@@ -33,7 +38,7 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
