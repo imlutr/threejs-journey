7f4de7c8da21b6fad33821706b6c6c1e9f0169c8
lutr <t.luca1152@gmail.com>
2024-06-04 22:24:35 +0300

    feat(05): animate using GSAP

--- 05-animations/src/script.js
+++ 05-animations/src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -29,17 +30,13 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
-// Clock
-const clock = new THREE.Clock()
+gsap.to(mesh.position, {duration: 1, delay: 1, x: 1})
+gsap.to(mesh.position, {duration: 1, delay: 2, x: 0})
 
 // Animations
 function tick() {
-  const elapsedTime = clock.getElapsedTime()
 
   // Update objects
-  camera.position.y = Math.sin(elapsedTime);
-  camera.position.x = Math.cos(elapsedTime);
-  camera.lookAt(mesh.position);
 
   // Render
   renderer.render(scene, camera)
