f4e298e9141a501ee8f1d81246ddd2930832c731
lutr <t.luca1152@gmail.com>
2024-06-23 19:45:23 +0300

    feat(07): make the canvas react to resizes

--- 07-fullscreen-and-resizing/src/script.js
+++ 07-fullscreen-and-resizing/src/script.js
@@ -65,4 +65,18 @@ const tick = () =>
     window.requestAnimationFrame(tick)
 }
 
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+});
+
 tick()
